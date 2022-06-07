4c7dc1b5e1cc07e378bb188b64cf6e2b7fea7df9
nicholasarinduque <89489431+nicholasarinduque@users.noreply.github.com>
2022-06-06 20:49:17 -0700

    3.4 done

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MoveLeft : MonoBehaviour
 {
-    private float speed = 30;
+    private float speed = 15;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);   
+        //transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
